d0bee2bb26425ac503aab965c9d948eab18fc03a
Russell Gooday <rpg_digital@yahoo.co.uk>
2022-07-14 10:50:32 +0100

    folder changes

--- section-06-pug/utils/path.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-
-/*
-    dirname strips off the end file in the filename
-    E:\webdev\Node JS\Section-05\shop.js
-    => E:\webdev\Node JS\Section-05
-*/
-module.exports = path.dirname(process.mainModule.filename)
--- section-07-mvc/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const productsController = require('../controllers/products')
-
-router.get('/add-product', productsController.getAddProduct)
-
-router.post('/add-product', productsController.postAddProduct)
-
-module.exports = router
