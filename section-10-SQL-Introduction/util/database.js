eb7f6c5a05b976989bb8cf4c781e8cb38d67b203
Russell Gooday <rpg_digital@yahoo.co.uk>
2022-10-20 10:47:20 +0100

    removed details

--- section-10-SQL-Introduction/util/database.js
+++ section-10-SQL-Introduction/util/database.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'russell',
-  password: 'sminkey2010',
-  database: 'node-complete'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 })
 
 module.exports = pool.promise()
